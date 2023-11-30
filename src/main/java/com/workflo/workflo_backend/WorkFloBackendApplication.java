f4dc37e69542c4a238b8c62c82e1c5893705927d
Banjo Oladele Samuel <o.samuel@native.semicolon.africa>
2023-11-30 13:01:10 +0100

    first commit

---
+++ src/main/java/com/workflo/workflo_backend/WorkFloBackendApplication.java
@@ -0,0 +1,13 @@
+package com.workflo.workflo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkFloBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkFloBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/workflo/workflo_backend/WorkFloBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workflo.workflo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkFloBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
